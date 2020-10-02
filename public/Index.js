1de277cfaffa64b3a4df36d48a44dbd3b4388dfe
Ana Carolina Carvalho <ancarolcs@gmail.com>
2020-10-02 09:24:38 -0300

    Index Refactoring

--- public/Index.js
+++ public/Index.js
@@ -5,47 +5,14 @@ let Kruskal =  require("./Kruskal")
 let fs = require('fs'),
     readline = require('readline');
 
-
-function readFile(fileName) {
-
-  let fileStream = fs.createReadStream(fileName),
-      rl,
-      data = '', 
-      index = 0,
-      gNodes = 0, 
-      gEdges = 0, 
-      gFrom = [],
-      gTo = [],
-      gWeight = [];
-  
-  fileStream.on('error', (err) => {
-    console.log('file issue: ', err.message)
-  });
-      
-  rl = readline.createInterface({
-      input: fileStream
-  });
-  // 'line' event - emitted whenever the input stream receives a new line \n
-  rl.on('line', (line) => {
-      data = line.split(' ');
-      if (index == 0) {
-          gNodes = parseInt(data[0], 10);
-          gEdges = parseInt(data[1], 10);
-      } else if (index <= gEdges) {
-          gFrom.push(data[0]);
-          gTo.push(data[1]);
-          gWeight.push(parseInt(data[2], 10));
-      }
-      index++;
-  });
-
   var kru = new Kruskal();
 
-  rl.on('close', () => {
+    let gNodes = 5;
+    let gEdges = 7;
+    let gFrom = ['MAD', 'MAD','MAD', 'MAD','XDT', 'OTP','FRA'];
+    let gTo = ['XDT', 'OTP','FRA', 'BER','OTP', 'FRA','BER'];
+    let gWeight = [2,3,4,4,3,4,2];
+
     if (gNodes && gEdges && gFrom.length && gTo.length && gWeight.length) {
       kru.krk(gNodes, gEdges, gFrom, gTo, gWeight);
-    } else console.log('invalid data file');
-  });
-}
-
-readFile('data1.txt');
+    } else console.log('invalid data');
